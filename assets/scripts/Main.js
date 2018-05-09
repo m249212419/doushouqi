a2ce50d8d53f26538996a4ebe5b087f6856ca654
马志鹏 <249212419@qq.com>
2018-05-09 21:00:48 +0800

    no message

--- assets/scripts/Main.js
+++ assets/scripts/Main.js
@@ -12,30 +12,18 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        loading: cc.Node
     },
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
 
     start () {
 
     },
 
     // update (dt) {},
+
+    onStartGame(event) {
+        var loading = this.loading.getCompent('Loading');
+        
+    },
+
 });
